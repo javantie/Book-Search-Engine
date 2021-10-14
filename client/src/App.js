46ce435e5d3ba9ca2742387c0db98cfec4591feb
Akeem C <javantieclarke@gmail.com>
2021-10-13 20:31:11 -0400

    added the schemas for the typeDefs and resolver

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+//Apollo setup
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
+
+// const client = new ApolloClient({
+//   request: operation => {
+//     const token = localStorage.getItem('id_token');
+
+//     operation.setContext({
+//       headers: {
+//         authorization: token ? `Bearer ${token}` : ''
+//       }
+//     });
+//   },
+//   uri: '/graphql'
+// });
+
 function App() {
   return (
+   
     <Router>
       <>
         <Navbar />
@@ -16,7 +35,9 @@ function App() {
         </Switch>
       </>
     </Router>
+   
   );
 }
 
 export default App;
+
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,33 @@
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Book {
+    authors: [String]
+    description: String
+    bookId: String
+    image: String
+    link: String
+    title: String
+  }
+
+  
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
